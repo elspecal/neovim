41cd0f50c9d672aaa11cd1b473fbade26f5701aa
csepella <csepella@tutanota.com>
2022-09-15 07:03:35 +0200

    trigger auto format via saving

--- lua/miki/autocmds.lua
+++ lua/miki/autocmds.lua
@@ -1,5 +1,13 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
+local au = vim.api.nvim_create_autocmd
+
+au("TextYankPost", {
   callback = function()
     vim.highlight.on_yank {}
+  end,
+})
+
+au("BufWritePre", {
+  callback = function()
+    vim.lsp.buf.format()
   end,
 })
