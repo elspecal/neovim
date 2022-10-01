e332054cd8e0246c8e6933249612ad65c6971004
Miklós Csepella <csepella@tutanota.com>
2022-10-01 22:08:11 +0200

    Change color schemem flavour.

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,32 +1,32 @@
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
 
 local ok_catppuccin, catppuccin = pcall(require, "catppuccin")
 if not ok_catppuccin then
-  print "failed to run catppuccin color scheme"
+  print("failed to run catppuccin color scheme")
   return
 end
-catppuccin.setup {}
+catppuccin.setup({})
 
 local set_scheme = function(scheme)
   vim.cmd("colorscheme " .. scheme)
 end
-set_scheme "catppuccin"
+set_scheme("catppuccin")
 
 local ok_autodark, auto_dark_mode = pcall(require, "auto-dark-mode")
 if not ok_autodark then
-  print "failed to run auto-dark-mode"
+  print("failed to run auto-dark-mode")
   return
 end
-auto_dark_mode.setup {
+auto_dark_mode.setup({
   set_dark_mode = function()
     vim.api.nvim_set_option("background", "dark")
-    vim.g.catppuccin_flavour = "macchiato"
-    set_scheme "catppuccin"
+    vim.g.catppuccin_flavour = "frappe"
+    set_scheme("catppuccin")
   end,
   set_light_mode = function()
     vim.api.nvim_set_option("background", "light")
     vim.g.catppuccin_flavour = "latte"
-    set_scheme "catppuccin"
+    set_scheme("catppuccin")
   end,
-}
-auto_dark_mode.init {}
+})
+auto_dark_mode.init()
