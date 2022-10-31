65f26cfaad31a16bf00c585d2f749477a7c8df13
Miklós Csepella <csepella@tutanota.com>
2022-10-31 16:21:00 +0100

    Shorten the alias for the local keymap setter.

--- lua/miki/maps.lua
+++ lua/miki/maps.lua
@@ -1,16 +1,22 @@
-local map = vim.keymap
+local map = vim.keymap.set
 
-map.set("i", "kj", "<Esc>", {})
-map.set("n", "<leader>w", "<Cmd>w<CR>", {})
-map.set("n", "<leader>q", "<Cmd>q<CR>", {})
-map.set("n", "<leader>wq", "<Cmd>wq<CR>", {})
-map.set("n", "<leader>ws", "<Cmd>w<CR><Cmd>so%<CR>", {})
-map.set("n", "<leader>e", "<Cmd>Neotree toggle<CR>", {})
 
-map.set("n", "<leader>ff", "<Cmd>Telescope find_files<CR>", {})
-map.set("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>", {})
-map.set("n", "<leader>fb", "<Cmd>Telescope buffers<CR>", {})
-map.set("n", "<leader>fh", "<Cmd>Telescope help_tags<CR>", {})
 
-map.set("n", "<leader>tt", "<Cmd>ToggleTerm<CR>", {})
-map.set("t", "kj", [[<C-\><C-n>]], {})
+
+map("i", "kj", "<Esc>", {})
+map("n", "<leader>w", "<Cmd>w<CR>", {})
+map("n", "<leader>q", "<Cmd>q<CR>", {})
+map("n", "<leader>wq", "<Cmd>wq<CR>", {})
+map("n", "<leader>ws", "<Cmd>w<CR><Cmd>so%<CR>", {})
+map("n", "<leader>e", "<Cmd>Neotree toggle<CR>", {})
+
+map("n", "<leader>ff", "<Cmd>Telescope find_files<CR>", {})
+map("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>", {})
+map({ "n", "v" }, "<leader>fG", "<Cmd>Telescope grep_string<CR>", {})
+map("n", "<leader>fb", "<Cmd>Telescope buffers<CR>", {})
+map("n", "<leader>fh", "<Cmd>Telescope help_tags<CR>", {})
+
+map("n", "<leader>tt", "<Cmd>ToggleTerm<CR>", {})
+map("n", "<leader>to", "<Cmd>Lspsaga open_floaterm<CR>", {})
+map("n", "<leader>tc", "<Cmd>Lspsaga close_floaterm<CR>", {})
+map("t", "kj", [[<C-\><C-n>]], {})
