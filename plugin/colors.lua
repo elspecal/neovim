2699e73e43a49e5c5f45a75a31328f94c5eaa920
Miklós Csepella <csepella@tutanota.com>
2022-10-31 14:46:43 +0100

    Change color scheme to terafox (nightfox).

--- plugin/colors.lua
+++ plugin/colors.lua
@@ -1,3 +1,6 @@
+local THEME_DARK = "terafox"
+local THEME_LIGHT = "dayfox"
+
 vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
 
 local ok_catppuccin, catppuccin = pcall(require, "catppuccin")
@@ -10,23 +13,24 @@ catppuccin.setup({})
 local set_scheme = function(scheme)
   vim.cmd("colorscheme " .. scheme)
 end
-set_scheme("catppuccin")
+set_scheme(THEME_DARK)
 
 local ok_autodark, auto_dark_mode = pcall(require, "auto-dark-mode")
 if not ok_autodark then
   print("failed to run auto-dark-mode")
   return
 end
+
 auto_dark_mode.setup({
   set_dark_mode = function()
     vim.api.nvim_set_option("background", "dark")
     vim.g.catppuccin_flavour = "frappe"
-    set_scheme("catppuccin")
+    set_scheme(THEME_DARK)
   end,
   set_light_mode = function()
     vim.api.nvim_set_option("background", "light")
     vim.g.catppuccin_flavour = "latte"
-    set_scheme("catppuccin")
+    set_scheme(THEME_LIGHT)
   end,
 })
 auto_dark_mode.init()
