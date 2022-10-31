296671a07dd9a1e066e859bd0a4760ceb99d6998
Miklós Csepella <csepella@tutanota.com>
2022-10-31 13:47:03 +0100

    Create autocmd to copy file change to remote.

--- lua/miki/commands.lua
+++ lua/miki/commands.lua
@@ -3,8 +3,25 @@ local usrcmd = vim.api.nvim_create_user_command
 
 au("TextYankPost", {
   callback = function()
-    vim.highlight.on_yank {}
+    vim.highlight.on_yank({})
   end,
+})
+
+local wremote = function()
+  local current = vim.fn.expand("%:p")
+  local user = "mcsepella"
+  local hostname = "mcsepella.dev.hearsaylabs.com"
+  local remotepath = string.gsub(current or "/Users/mcsepella/", "^/Users", "/home")
+  local command = string.format("scp -Cpq %s %s", current, user .. "@" .. hostname .. ":" .. remotepath)
+
+  io.popen(command)
+  vim.api.nvim_input("<CR>")
+  print(command)
+end
+
+au("BufWritePost", {
+  pattern = os.getenv("HOME") .. "/projects/HearsayLabs*",
+  callback = wremote,
 })
 
 local set_indent = function(opts)
@@ -15,3 +32,8 @@ local set_indent = function(opts)
 end
 
 usrcmd("SetIndent", set_indent, { nargs = 1 })
+
+au("FileType", {
+  pattern = { "lua" },
+  command = "SetIndent 2",
+})
