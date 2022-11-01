519ad19f89fd87e236b5051bd7c8fc0b6fddc608
Miklós Csepella <csepella@tutanota.com>
2022-11-01 02:53:43 +0100

    Disable pycodestyle for the moment.

--- lua/miki/configs/null-ls.lua
+++ lua/miki/configs/null-ls.lua
@@ -1,23 +1,23 @@
 local ok, nls = pcall(require, "null-ls")
 if not ok then
-  print("failed to load null-ls")
-  return
+	print("failed to load null-ls")
+	return
 end
 
 nls.setup({
-  sources = {
-    nls.builtins.code_actions.gitsigns,
-    nls.builtins.diagnostics.eslint_d,
-    nls.builtins.diagnostics.flake8,
-    nls.builtins.diagnostics.golangci_lint,
-    nls.builtins.diagnostics.yamllint,
-    nls.builtins.formatting.black,
-    nls.builtins.formatting.gofumpt,
-    nls.builtins.formatting.goimports,
-    nls.builtins.formatting.prettierd,
-    nls.builtins.formatting.eslint_d,
-    nls.builtins.formatting.stylua,
-    nls.builtins.formatting.beautysh,
-    nls.builtins.formatting.yamlfmt,
-  },
+	sources = {
+		nls.builtins.code_actions.gitsigns,
+		nls.builtins.diagnostics.eslint_d,
+		nls.builtins.diagnostics.golangci_lint,
+		nls.builtins.diagnostics.mypy.with({ prefer_local = true }),
+		-- nls.builtins.diagnostics.pycodestyle.with({ prefer_local = true }),
+		nls.builtins.diagnostics.pylint.with({ prefer_local = true }),
+		nls.builtins.diagnostics.yamllint,
+		nls.builtins.formatting.gofumpt,
+		nls.builtins.formatting.goimports,
+		nls.builtins.formatting.eslint_d,
+		nls.builtins.formatting.stylua,
+		nls.builtins.formatting.beautysh,
+		nls.builtins.formatting.yamlfmt,
+	},
 })
